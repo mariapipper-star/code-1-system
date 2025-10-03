02b2547764598cc67405760dc7969e0a5de4bd09
mariapipper-star <maria.pipper@escola.pr.gov.br>
2025-10-03 07:43:31 -0300

    Add files via upload

---
+++ sequencia-ecf.py
@@ -0,0 +1,8 @@
+print("***Mini questionário***\n")
+nome = input("Qual seu nome?\n")
+idade = input("Qual a sua idade?\n")
+genero = input("Você é homem ou mulher?\n")
+endereco = input("Qual seu endereço?\n")
+
+print("Olá,", nome,"\n")
+print("\nsss")
